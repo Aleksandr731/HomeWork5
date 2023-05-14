b17f1db0eca25287545f1d04b2474f69993ddf27
Александр <alexst731w@gmail.com>
2023-05-14 15:38:20 +0300

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -36,8 +36,8 @@ public class Main {
 
     public static void task3() {
         System.out.println("Задача 3");
-        int year =2021;
-        if (year%4 == 0 && year%400 == 0 || year%100 != 0){
+        int year =2104;
+        if (year%4 == 0 && year%100 != 0 || year%400 == 0){
             System.out.println(year + " год является високосным");
         } else {
             System.out.println(year + " год не является високосным");
