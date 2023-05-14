cd2dae0d77a0353acad21eea988e55abd28ebefb
Александр <alexst731w@gmail.com>
2023-05-14 12:08:26 +0300

    Задача 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+
+
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        int clientOS = 1;
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else  {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    }
+
+}
